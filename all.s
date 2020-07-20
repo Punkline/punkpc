146397ae658c0240d43e13378a350099fa997d44
Punkline <bourgeoizagna@gmail.com>
2020-07-20 05:24:19 -0700

    update all.s
    add recent additions include list

--- all.s
+++ all.s
@@ -2,9 +2,9 @@
 .include "./punkpc/dbg.s"
 .include "./punkpc/enum.s" # includes ifdef
 .include "./punkpc/idxr.s" # include xev
-.include "./punkpc/ifalt.s"
 .include "./punkpc/ld.s" # includes xem
 .include "./punkpc/smallints.s"
 .include "./punkpc/stacks.s" # includes sidx
-
+.include "./punkpc/bcount.s"
+.include "./punkpc/str.s" # includes ifalt
 /**/
