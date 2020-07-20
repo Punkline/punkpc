8d31dd6dd1f9e31da1b6b63de1a5d441cf71e5da
Punkline <bourgeoizagna@gmail.com>
2020-07-20 05:13:34 -0700

    ifalt version 0.0.2
    rewrote main method, added a new 'ifalt.reset' method, and added better documentation

--- _doc/ifalt_doc.s
+++ _doc/ifalt_doc.s
@@ -1,22 +1,8 @@
-/*## Header:
 # --- ifalt - a method for checking if calls are in .altmacro mode 'alt' or .noaltmacro mode 'nalt'
 
-##*/
-/*## Attributes:
-# --- Class Properties ---
 
-# --- alt  - bool is True if altmacro mode is detected, else False
-# --- nalt - not alt -- inverse of alt
-# these globals can be used as evaluable properties in .if statements
+# --- Example use of the ifalt module:
 
-
-# --- Class Methods ---
-
-# --- ifalt
-# Checks altmacro mode, and updates 'alt', 'nalt' properties accordingly
-
-##*/
-/*## Examples:
 .include "./punkpc/ifalt.s"
 
 .macro myStringMacro, str;  .error "\str";  .endm
@@ -104,14 +90,15 @@ ifalt.reset myAltMemory
 .endif;
 # >>> Error: Altmacro Mode is Disabled
 
-##*/
+
+# --- Module attributes:
+# --- Class Properties ----------------------------------------------------------------------------
+
+# --- alt  - bool is True if altmacro mode is detected, else False
+# --- nalt - not alt -- inverse of alt
+# these globals can be used as evaluable properties in .if statements
 
 
-.ifndef ifalt.included; ifalt.included = 0; .endif; .ifeq ifalt.included; ifalt.included = 2
-# version 0.0.2:
-# - rewrote ifalt to not use a bogus argument, and in a way that won't conflict with other symbols
-# - added 'ifalt.reset' to easily implement a reset method using generated 'alt' bool
+# --- ifalt
+# Checks altmacro mode, and updates 'alt', 'nalt' properties accordingly
 
-  .macro ifalt;.irp alt_check,%1;alt=0;.ifc \alt_check,1;alt=1;.endif;nalt=alt^1;.endr;.endm
-  .macro ifalt.reset, reset_alt=alt; .if \reset_alt;.altmacro;.else;.noaltmacro;.endif;ifalt;.endm
-.endif
