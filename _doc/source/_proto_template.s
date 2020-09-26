764c440359cf7640f4eeac17b29af3613dafb492
Punkline <bourgeoizagna@gmail.com>
2020-09-25 19:29:03 -0700

    template file, for making new class modules

---
+++ _doc/source/_proto_template.s
@@ -0,0 +1,27 @@
+/*## Header:
+# --- title
+# desc
+# - detail
+
+##*/
+/*## Attributes:
+
+# --- Class Properties ---
+
+# --- Constructor Method ---
+
+  # --- Object Properties ---
+
+  # --- Object Methods ---
+
+# --- Class Methods ---
+
+##*/
+/*## Examples:
+
+##*/
+.ifndef class.included; class.included = 0; .endif; .ifeq class.included; class.included = 1
+
+  # --- Class module
+
+.endif
