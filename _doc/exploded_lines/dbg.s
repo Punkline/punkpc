a09689eab734609eca638aa436146d33adfb4f55
Punkline <bourgeoizagna@gmail.com>
2021-01-03 15:17:25 -0800

    dbg 0.0.1 - Debug Expression Tool
    # --- Debug expression
    # simply prints an error "[expression] = (evaluation)" -- useful for debugging without a debugger
    
    # version 0.0.2
    # - changed '.error' directive into a '.warning' directive
    # version 0.0.1
    # - added to punkpc module library

---
+++ _doc/exploded_lines/dbg.s
@@ -0,0 +1,16 @@
+.ifndef punkpc.library.included
+  .include "punkpc.s"
+.endif;
+punkpc.module dbg, 2
+.if module.included == 0
+  .macro dbg,  i,  x
+    .ifb \x
+      .altmacro
+      dbg %\i, \i
+      .noaltmacro
+    .else;
+      .warning "\x = \i"
+    .endif;
+  .endm;
+.endif;
+
