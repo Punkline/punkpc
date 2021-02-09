326bfc361bc5a5f5c646e6d14f02827c61e77de8
Punkline <bourgeoizagna@gmail.com>
2021-02-09 11:52:02 -0800

    scratch hex - working objects
    alignment syntax even seems to work as intended, may add quote feature. Still need to document final attribute notes

--- src/scratchpad/hex.s
+++ src/scratchpad/hex.s
@@ -74,13 +74,14 @@ hex 1...
 hex 1.........
 # >>> hex 1......
 # If more than 6 sequential '.' chars are detected, it will be capped at 6
-# - this produces 32-byte alignment
+# - this produces a max of 32-byte alignment
 
 
 hex 10.
 # >> 10
-# alignments will not b
-
+# alignments will not add padding if the counter is already aligned
+# - the counter is relative to byte index 0 of the buffer
+# - as a stack, this index is represented by the '.s' property
 
 
 
@@ -137,12 +138,110 @@ punkpc.module hex, 1
   .macro hex.new, self, va:vararg
     .ifnb \self
       enc.new \self
-      stack.purge_hook \self, hex
-      .macro \self, va:vararg; stack.call_mut \self, hex, default, \va; .endm
-      # extend the 'enc' object, which extends the 'stack' object
+      .if obj.ndef
+        stack.purge_hook \self, hex, emit_bytes
+        stack.meth \self, emit_bytes
+        .macro \self, varg:vararg; stack.call_mut \self, hex, default, \varg; .endm
+        # extend the 'enc' object, which extends the 'stack' object
 
+        \self\().mode push, hex
+        # special mode is used to filter inputs when pushing bytes through the encoder
+
+        \self\().pushing_hex = 0
+        \self\().emit = 1
+        \self\().i = 0
+        # extended properties
+
+        \self\().__nib = 0
+        \self\().__byte = 0
+        \self\().__align = 0
+        # hidden properties (for parse state machine)
+
+      .endif
     .endif
-  .endm
+  .endm; .macro stack.mut.hex.default, self, va:vararg
+    \self\().pushing_hex = 1; \self\().i = \self\().s
+    hex.__altm = alt; ifalt; hex.__alt = alt; .noaltmacro
+    .irp arg, \va;
+      .ifnb \arg;
+        \self\().enc \arg;
+      .endif;
+    .endr
+    # encoder will invoke mutated stack push behavior
+
+    hex.__flush_escaped \self
+    ifalt.reset hex.__alt; alt = hex.__altm
+    \self\().pushing_hex = 0
+    # wrapper flag induces special behavior instead of default
+
+    .if \self\().emit; \self\().emit_bytes \self\().i; .endif
 
 
+  .endm; .macro stack.mut.emit_bytes.default, self, beg, end, va:vararg
+    .ifb \beg; hex.__emit_beg = \self\().q; .else; hex.__emit_beg = \beg; .endif
+    .ifb \end; hex.__emit_end = \self\().s-1; .else; hex.__emit_end = \end; .endif
+    stack.rept_range \self, hex.__emit_beg, hex.__emit_end, .byte
+
+  .endm; .macro stack.mut.push.hex, self, char, va:vararg
+    .if \self\().pushing_hex == 0;
+      stack.mut.push.default \self, \char, \va; .exitm;
+    .endif
+    # default is used when pushing normally
+
+    .if \self\().pushing_hex == 1
+      .if \char == 0x30; \self\().pushing_hex = 2
+      .else; hex.__check_escape \self, \char; .endif
+    .else; hex.__escaping \self, \char; .endif
+    # this preliminary check handles dispatching to
+
+  .endm; .macro hex.__flush_escaped, self
+    .if \self\().pushing_hex == 2; hex.__check_escape \self, 0x30
+    .elseif \self\().pushing_hex == 3
+      .if hex.__align > 1
+        hex.__align = (1<<(hex.__align-1))
+        hex.__align = (hex.__align - \self\().s) & (hex.__align-1)
+        .rept hex.__align
+          stack.mut.push.default \self, 0
+        .endr; hex.__align = 0
+      .elseif \self\().__nib; hex.__nibble \self, 0; .endif
+    .endif; \self\().pushing_hex = 1
+    # this resets back to normal hex pushing mode, after one of the escaping modes is done
+
+  .endm; .macro hex.__check_escape, self, char
+    .if \char == 0x2E; hex.__escape_dot \self
+    .elseif ((\char >= 0x30) && (\char <= 0x39)); hex.__nibble \self, \char - 0x30
+    .elseif ((\char >= 0x41) && (\char <= 0x46)); hex.__nibble \self, \char - 0x37
+    .elseif ((\char >= 0x61) && (\char <= 0x66)); hex.__nibble \self, \char - 0x57
+    .endif # this handles a filter for only pushing hex digits to the nibble buffer method
+
+  .endm; .macro hex.__nibble, self, val
+    \self\().__byte = (\self\().__byte << 4) | (\val)
+    .if \self\().__nib; stack.mut.push.default \self, (\self\().__byte & 0xFF); .endif
+    \self\().__nib = \self\().__nib ^ 1
+    # this creates a push buffer that combines 2 nibbles at a time to generate bytes
+
+  .endm; .macro hex.__escaping, self, char
+    .if \self\().pushing_hex == 2
+      .if ((\char == 0x78) || (\char == 0x58));
+        \self\().pushing_hex = 1
+        # skip over '0x' or '0X' combinations
+
+      .else;
+        hex.__flush_escaped \self
+
+        .if \char == 0x30; \self\().pushing_hex = 2
+        .else; hex.__check_escape \self, \char; .endif
+      .endif
+    .elseif \self\().pushing_hex == 3
+      .if \char == 0x2E; hex.__escape_dot \self
+      # accumulate alignment dots
+
+      .else; hex.__flush_escaped \self
+      .endif
+    .endif
+
+  .endm; .macro hex.__escape_dot, self
+    .if \self\().__align < 6; \self\().__align = \self\().__align + 1; .endif
+    \self\().pushing_hex = 3
+  .endm; hex.new hex
 .endif
